7653df9bd50ac5559c3f92d5b51f718afca334b9
Armagan Kimyonoglu <akimyonoglu@gmail.com>
2014-06-10 13:45:45 +0300

    batch CSR maker script

--- bash/safe_mop.sh
+++ bash/safe_mop.sh
@@ -2,8 +2,7 @@
 # author: Armagan Kimyonoglu
 # rm files and directories safely by checking open files
 
-if [ -z $1 ];
-then
+if [ -z $1 ]; then
   echo "usage: ./safe_mop.sh /tmp # safely clean tmp directory"
   exit
 fi
@@ -11,19 +10,16 @@ fi
 PARENT_DIR=$1
 
 filter_and_remove() {
-  if [ -d $1 ];
-  then
+  if [ -d $1 ]; then
     for i in `ls -A1 $1`;
     do
       filter_and_remove $1/$i
     done
-    if [ "$PARENT_DIR" != "$1" ];
-    then
+    if [ "$PARENT_DIR" != "$1" ]; then
       echo "[INFO] trying to safely rm directory $1"
       rmdir $1
     fi
-  elif sudo lsof $1;
-  then
+  elif sudo lsof $1 &>/dev/null; then
     echo "[WARN] cannot rm $1 (resource busy)"
   else
     echo "[INFO] trying to safely rm $1"
